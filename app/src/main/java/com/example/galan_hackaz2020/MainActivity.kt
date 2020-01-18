f59571c25d05fe53949c8b863293cc7cee645b77
mfgal20 <mfgalan10@gmail.com>
2020-01-18 02:14:15 -0700

    added splash screen function

--- app/src/main/java/com/example/galan_hackaz2020/MainActivity.kt
+++ app/src/main/java/com/example/galan_hackaz2020/MainActivity.kt
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
+    val splashtime = 4000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+
 }
