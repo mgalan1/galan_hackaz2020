ae520baded95a85fe8e86aec664cc7caaa7e6633
mfgal20 <mfgalan10@gmail.com>
2020-01-18 13:46:03 -0700

    add create acct view

--- app/src/main/java/com/example/galan_hackaz2020/MainActivity.kt
+++ app/src/main/java/com/example/galan_hackaz2020/MainActivity.kt
@@ -2,12 +2,21 @@ package com.example.galan_hackaz2020
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var user: TextView
+    lateinit var pass: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun setUserInfo(){
+        // TODO validate user exists in firebase
+        user = findViewById(R.id.user)
+        pass = findViewById(R.id.pass)
     }
 
 
